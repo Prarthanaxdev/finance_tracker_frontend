98c9a37923d75ceb39fecf4fc92bcdbb1e97e77d
Prarthanaxdev <Prarthanab@mindfiresolutions.com>
2026-01-14 00:09:51 +0530

    Adding the env files

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiBaseUrl: 'http://localhost:5000',
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
   apiTimeout: 10000,
-  isDev: true,
+  isDev: import.meta.env.DEV,
 } as const;
